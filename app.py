cf9433ab3e54a6688963d9721cc6f76045e4f780
debnathsanjit665-droid <debnathsanjit665@gmail.com>
2026-03-05 12:13:13 +0530

    os update

--- app.py
+++ app.py
@@ -125,3 +125,9 @@ def reset():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
